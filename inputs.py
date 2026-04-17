bb4605f677f57aaebf5696638b9642e0619f6e6c
rhytidia <264509465+rhytidia@users.noreply.github.com>
2026-04-17 13:17:06 -0700

    First draft of main.py plus inputs

---
+++ inputs.py
@@ -0,0 +1,15 @@
+
+q1 = "What are you working on?"
+q2 = "What have you completed or accomplished?"
+q3 = "Any questions or challenges coming up?"
+q4 = "What are your next steps?"
+questions = [q1, q2, q3, q4]
+
+welcome_msg = '''\nWelcome to your coding log. 
+First, you will choose a path for the markdown file where your log will be saved. 
+Then you will answer questions and your answers will be saved to the log.\n'''
+
+questions_msg = '''\nYou file will be saved at that path.
+Now, please answer the questions as they come up.
+If you want to skip to the next question just hit 'return.'
+If you want to exit early, type 'end'. In that case, only the answers you've provided so far will be saved to the log.\n'''
